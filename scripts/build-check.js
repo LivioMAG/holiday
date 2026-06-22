6a11bb13b2454eab904cf739e0697c19a671469f
LivioMAG <sursee.marechauxag@gmail.com>
2026-06-22 14:21:49 +0200

    Build Between Us mobile webapp

---
+++ scripts/build-check.js
@@ -0,0 +1,3 @@
+import { readFileSync } from 'node:fs'
+for (const file of ['index.html','styles/base.css','styles/app.css','src/app.js']) readFileSync(file, 'utf8')
+console.log('Build check completed: static assets are present.')
